ebe175ea6cff0e5bf89e42697616864c5eab2ae5
Manas Singh <manassingh557@gmail.com>
2020-12-04 14:58:33 +0530

    First Commit

---
+++ app/src/main/java/com/example/midterm/MyListAdapter.java
@@ -0,0 +1,59 @@
+package com.example.midterm;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder> {
+
+    private MyListData[] listdata;
+
+    // RecyclerView recyclerView;
+    public MyListAdapter(MyListData[] listdata) {
+        this.listdata = listdata;
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem= layoutInflater.inflate(R.layout.list_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(listItem);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final MyListData myListData = listdata[position];
+        holder.textView.setText(listdata[position].getDescription());
+        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),"click on item: "+myListData.getDescription(),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return listdata.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+        public LinearLayout linearLayout;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.textView = (TextView) itemView.findViewById(R.id.textView);
+            linearLayout = (LinearLayout)itemView.findViewById(R.id.linearLayout);
+        }
+    }
+
+
+}
